48a7bd88ae3672d17c7815ad5796fa39c6cd1c7d
Rsl1122 <djrapitops@hotmail.com>
2018-11-18 15:47:48 +0200

    Week 3 ex 8

--- LoginCucumber/src/main/java/ohtu/io/ConsoleIO.java
+++ LoginCucumber/src/main/java/ohtu/io/ConsoleIO.java
@@ -1,11 +1,15 @@
 
 package ohtu.io;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.Scanner;
 
+@Component
 public class ConsoleIO implements IO {
     private Scanner scanner = new Scanner(System.in);
-    
+
     public void print(String toPrint) {
         System.out.println(toPrint);
     }
